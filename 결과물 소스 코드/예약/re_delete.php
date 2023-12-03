9004206e13365f113ca06104459a9279575a0f42
chanhyung01 <151607608+chanhyung01@users.noreply.github.com>
2023-12-03 16:35:58 +0900

    Delete 결과물 소스 코드 directory

--- 결과물 소스 코드/예약/re_delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $c_id = $_REQUEST["c_id"];
-    
-    require("db_connect.php");
-    $db->exec("delete from c_reservation where c_id=$c_id");
-    
-    header("Location:location_table.php");
-    exit();
-?>
